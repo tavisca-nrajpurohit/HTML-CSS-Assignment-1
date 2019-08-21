253b8101e41c675a0e1bbd7c9a3e0f6871923d4b
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-08-21 07:31:53 +0530

    Added JS for 'Update todo list item functionalit

--- main.js
+++ main.js
@@ -5,18 +5,18 @@ function addtolist(e) {
     let formData = document.getElementById("form-todo");
     let todoTable = document.getElementById("todo-list");
     let tablerow = document.createElement('tr');
-    tablerow.id = todoIndex;
     let tabledata1 = document.createElement('td');
     tabledata1.innerHTML = ">>";
     tablerow.append(tabledata1);
     let tabledata2 = document.createElement('td');
+    tabledata2.id = todoIndex
     tabledata2.innerHTML = formData.value;
     tablerow.append(tabledata2);
     let tabledata3 = document.createElement('td');
-    tabledata3.innerHTML = '<button id="' + todoIndex + '" class="delete" onclick="deleteFromList(event,this.id)">Delete</button>';
+    tabledata3.innerHTML = '<button class="delete" onclick="deleteFromList(event,' + todoIndex + ')">Delete</button>';
     tablerow.append(tabledata3);
     let tabledata4 = document.createElement('td');
-    tabledata4.innerHTML = '<button id="' + todoIndex + '" class="edit" onclick="deleteFromList(event,this.id)">Edit</button>';
+    tabledata4.innerHTML = '<button class="edit" onclick="updateListItem(event,' + todoIndex + ')">Edit</button>';
     tablerow.append(tabledata4);
     if (formData.value != "") {
         todoTable.appendChild(tablerow);
@@ -27,7 +27,12 @@ function addtolist(e) {
 
 function deleteFromList(e, id) {
     e.preventDefault();
-    let todoTable = document.getElementById("todo-list");
-    let tablerow = document.getElementById(id);
-    tablerow.parentNode.removeChild(tablerow);
+    let tabledata = document.getElementById(id);
+    tabledata.parentElement.parentElement.removeChild(tabledata.parentElement);
+}
+
+function updateListItem(e, id) {
+    e.preventDefault();
+    let tabledata = document.getElementById(id);
+    tabledata.innerHTML = prompt("Update To-do list Item", document.getElementById('form-todo').value)
 }
