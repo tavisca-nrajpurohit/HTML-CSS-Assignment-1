5df7d2b9d64d330eb74c9ac8b13f7a9451ae3185
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-08-22 11:59:55 +0530

    Added Click-to-edit feature on profile page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let todoIndex = 1;
 
+// Adding content to To-Do List 
 function addtolist(e) {
     e.preventDefault();
     let formData = document.getElementById("form-todo");
@@ -26,18 +27,21 @@ function addtolist(e) {
     resetColor();
 }
 
+// Delete Content From To-Do List
 function deleteFromList(e, id) {
     e.preventDefault();
     let tabledata = document.getElementById(id);
     tabledata.parentElement.parentElement.removeChild(tabledata.parentElement);
 }
 
+// Update Content in the To-do List
 function updateListItem(e, id) {
     e.preventDefault();
     let tabledata = document.getElementById(id);
     tabledata.innerHTML = prompt("Update To-do list Item", document.getElementById('form-todo').value)
 }
 
+// Reset colors after search process  
 function resetColor() {
     for (var i = 0; i < todoIndex; i++) {
         let temp = document.getElementById(i);
@@ -48,6 +52,7 @@ function resetColor() {
     }
 }
 
+// Search among the content of todolist and highlight the matching content
 function search() {
     let formData = document.getElementById("form-todo");
     let str = formData.value;
@@ -63,5 +68,31 @@ function search() {
                 temp.parentElement.style.backgroundColor = "";
             }
         }
+    }
+}
+
+
+// Profile click to update content
+function updateProfile(elem) {
+    let inputElementId = elem.id + "-input";
+    let inputElement = document.getElementById(inputElementId);
+    let oldData = elem.innerHTML;
+    inputElement.value = oldData;
+    elem.style.display = "none";
+    inputElement.style.display = "block";
+}
+
+function saveData(event, inputElement) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        console.log("Function saveData triggered !");
+        newData = inputElement.value;
+        let elemId = inputElement.id;
+        elemId = elemId.replace("-input", "");
+        let elem = document.getElementById(elemId);
+        elem.innerHTML = newData;
+
+        elem.style.display = "block";
+        inputElement.style.display = "none";
     }
 }
