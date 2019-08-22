5250752e107aec378bc0d5f2f7f94860745797cd
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-08-22 14:21:38 +0530

    Added code for marking todo list item as Done or Pending

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 let todoIndex = 1;
 
-// Adding content to To-Do List 
-function addtolist(e) {
-    e.preventDefault();
-    let formData = document.getElementById("form-todo");
+// Fetching content from External API - JSON-Placeholder
+// To limit the number of data elements to be fetched => set variable "NumberOfDataElements" below
+const NUMBER_OF_ELEMENTS = 10;
+
+
+function initialSetup() {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then(function(data) {
+            for (let i = 0; i < NUMBER_OF_ELEMENTS; i++) {
+                stringToHtml(data[i].title);
+            }
+        });
+}
+
+// Filling up To-do List with Dummy Data from Api
+initialSetup();
+
+// adding string to to-do list
+function stringToHtml(str) {
     let todoTable = document.getElementById("todo-list");
     let tablerow = document.createElement('tr');
     let tabledata1 = document.createElement('td');
@@ -11,7 +27,8 @@ function addtolist(e) {
     tablerow.append(tabledata1);
     let tabledata2 = document.createElement('td');
     tabledata2.id = todoIndex;
-    tabledata2.innerHTML = formData.value;
+    tabledata2.innerHTML = str;
+    MarkStatusUnDone(tabledata2);
     tablerow.append(tabledata2);
     let tabledata3 = document.createElement('td');
     tabledata3.innerHTML = '<button class="delete" onclick="deleteFromList(event,' + todoIndex + ')">Delete</button>';
@@ -19,12 +36,34 @@ function addtolist(e) {
     let tabledata4 = document.createElement('td');
     tabledata4.innerHTML = '<button class="edit" onclick="updateListItem(event,' + todoIndex + ')">Edit</button>';
     tablerow.append(tabledata4);
-    if (formData.value != "") {
+    if (str != "") {
         todoTable.appendChild(tablerow);
-        formData.value = "";
+        str = "";
     }
     todoIndex++;
     resetColor();
+}
+
+function MarkStatusDone(elem) {
+    elem.style.textDecoration = "line-through";
+    elem.setAttribute("onclick", "MarkStatusUnDone(this);");
+}
+
+function MarkStatusUnDone(elem) {
+    elem.style.textDecoration = "";
+    elem.setAttribute("onclick", "MarkStatusDone(this);");
+}
+
+// Adding content to To-Do List 
+function addtolist(e) {
+    e.preventDefault();
+    let formData = document.getElementById("form-todo");
+    let str = formData.value;
+    // Removing extra "spaces" in the todo list item.
+    str = str.trim();
+
+    stringToHtml(str);
+    formData.value = "";
 }
 
 // Delete Content From To-Do List
@@ -38,7 +77,11 @@ function deleteFromList(e, id) {
 function updateListItem(e, id) {
     e.preventDefault();
     let tabledata = document.getElementById(id);
-    tabledata.innerHTML = prompt("Update To-do list Item", document.getElementById('form-todo').value)
+    let message = prompt("Update To-do list Item", tabledata.innerHTML);
+    if (message == "null" || message == null || message == "") {;
+    } else {
+        tabledata.innerHTML = message;
+    }
 }
 
 // Reset colors after search process  
@@ -62,7 +105,6 @@ function search() {
 
         if (temp != null) {
             if (temp.innerHTML.includes(str) && str != "") {
-                console.log("temp = " + temp.innerHTML);
                 temp.parentElement.style.backgroundColor = "yellow";
             } else {
                 temp.parentElement.style.backgroundColor = "";
